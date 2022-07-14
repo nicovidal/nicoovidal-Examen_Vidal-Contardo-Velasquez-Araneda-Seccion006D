23ec011b71111712ed8e7356c7a66e8c76f130fb
Nicolas Vidal <nico.vidal.m@gmail.com>
2022-07-14 01:40:52 -0400

    detalles finales

--- Kojo/views.py
+++ Kojo/views.py
@@ -14,7 +14,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.views.decorators.csrf import csrf_exempt
-import requests
 
 @csrf_exempt
 @api_view(['GET', 'POST'])
